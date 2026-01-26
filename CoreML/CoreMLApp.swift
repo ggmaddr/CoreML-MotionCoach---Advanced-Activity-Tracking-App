5245e371db9cff92b6a71430efb6511be3fe35b7
Grady Ta <giang.ta@centricsoftware.com>
2026-01-26 02:19:28 -0800

    Initial Commit

---
+++ CoreML/CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLApp.swift
+//  CoreML
+//
+//  Created by Grady Ta on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
